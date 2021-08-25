abb62a42ffb2de6c4b5ca1360ceb8ba9c2e4cec1
ivanganev <ivan.ganev@leanplum.com>
2021-08-26 00:41:54 +0200

    Enable torrent parsing

--- src/main/scala/EntryPoint.scala
+++ src/main/scala/EntryPoint.scala
@@ -1,72 +1,31 @@
+import Torrent.torrentDecoder
 import benc._
 import cats.effect.{IO, IOApp, Resource}
 import scodec.bits.BitVector
 
 import java.io.{File, FileInputStream}
-import scala.jdk.CollectionConverters._
-
-import Torrent.torrentDecoder
 
 object EntryPoint extends IOApp.Simple {
-//  def run: IO[Unit] = {
-//    val bencode = new Bencode(true)
-//    val file = new File("ubuntu.torrent")
-//    val inputStreamResource = createInputStream(file)
-//
-//    val dictionaryResource =
-//      inputStreamResource.map(inputStream => {
-//        val bytes = inputStream.readAllBytes()
-//        val dictionary = bencode.decode(bytes, Type.DICTIONARY)
-//        dictionary.asScala.toMap
-//      })
-//    //dictionaryResource.map(dictionary => dictionary.get("info")).use(IO.println)
-//    dictionaryResource
-//      .map(dictionary => dictionary.get("info") match {
-//        case Some(x@java.util.LinkedHashMap) => x.asScala.toMap
-//        case None => Map[String, AnyRef]()
-//      })
-//      .map(infoDictionary => infoDictionary.get("length"))
-//      .use(IO.println)
-//  }
 
   def run: IO[Unit] = {
     val file = new File("ubuntu.torrent")
-//    val inputStream = new FileInputStream(file)
-//    val bytes = inputStream.readAllBytes()
-//    val bitVector = BitVector(bytes)
-//
-//    val x = Benc.fromBenc[Torrent](bitVector)
-//
-//    println(x)
-//
-//    IO ()
+    val inputStreamResource = createInputStreamResource(file)
 
-    for {
-      inputStream <- createInputStream(file)
-      torrent <- {
-        val bytes = inputStream.readAllBytes()
-        val bitVector = BitVector(bytes)
-        Benc.fromBenc[Torrent](bitVector).toOption
-      }
-    } yield torrent
-//
-//    val dictionaryResource =
-//      inputStreamResource.(inputStream => {
-//        val bytes = inputStream.readAllBytes()
-//        val bitVector = BitVector(bytes)
-//        val dictionary = BDecoder[Torrent].decode(bitVector)
-//        dictionary
-//      })
-//    //dictionaryResource.map(dictionary => dictionary.get("info")).use(IO.println)
-//    dictionaryResource
-//      .map(dictionary => dictionary.get("info") match {
-//        case Some(x@java.util.LinkedHashMap) => x.asScala.toMap
-//        case None => Map[String, AnyRef]()
-//      })
-//      .map(infoDictionary => infoDictionary.get("length"))
-//      .use(IO.println)
+    inputStreamResource.use { inputStream =>
+      for {
+        bytes <- IO.blocking(inputStream.readAllBytes())
+        torrent <- parseTorrentFromBytes(bytes)
+        _ <- IO.println(torrent)
+      } yield ()
+    }
   }
 
-  def createInputStream(f: File): Resource[IO, FileInputStream] =
+  def parseTorrentFromBytes(bytes: Array[Byte]): IO[Torrent] = {
+    val bitVector = BitVector(bytes)
+    val torrentEither = Benc.fromBenc[Torrent](bitVector)
+    IO.fromEither(torrentEither)
+  }
+
+  def createInputStreamResource(f: File): Resource[IO, FileInputStream] =
     Resource.fromAutoCloseable(IO(new FileInputStream(f)))
 }
