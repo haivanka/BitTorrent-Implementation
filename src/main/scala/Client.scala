6f3cee108371247ccfd170b135b1bcd69c425e83
ivanganev <ivan.ganev@leanplum.com>
2021-08-31 22:14:01 +0200

    send handshake

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -3,17 +3,24 @@ import cats.effect.{IO, Ref}
 import cats.instances.seq._
 import cats.syntax.parallel._
 
+import scala.concurrent.duration.DurationInt
+
 object Client {
   def start(torrent: Torrent): IO[Unit] = {
     for {
       announceResponse <- Tracker.sendFirstAnnounce(torrent)
+      _ <- IO.println(announceResponse.peerAddresses)
       activePeersRef <- Ref.of[IO, Set[PeerAddress]](announceResponse.peerAddresses)
-      peerAddresses <- activePeersRef.get.map(_.toSeq.take(5))
+      peerAddresses <- activePeersRef.get.map(_.toSeq)
       _ <- IO.println(peerAddresses)
       peerCommunicationResources = peerAddresses.map(pa => PeerCommunication.apply(pa, activePeersRef))
       handshake = Handshake(torrent.info.infoHash, Tracker.defaultPeerId)
       workers = peerCommunicationResources.map(r => r.use(pc => pc.start(handshake)))
       _ <- workers.parSequence
+      _ <- activePeersRef.get.map(println)
+      _ <- IO.sleep(10.seconds)
+      _ <- activePeersRef.get.map(println)
+      _ <- IO.sleep(10.seconds)
     } yield ()
   }
 }
--- src/main/scala/PeerCommunication.scala
+++ src/main/scala/PeerCommunication.scala
@@ -1,20 +1,43 @@
 import Codecs.handshakeCodec
 import NetworkUtil.Handshake
+import PeerCommunication.{convertAttemptToIO, convertHandshakeToChunk}
 import cats.effect.{IO, Ref, Resource}
 import com.comcast.ip4s.SocketAddress
 import fs2.Chunk
 import fs2.io.net.{Network, Socket}
+import scodec.Attempt
 
 case class PeerCommunication(socket: Socket[IO]) {
-  def start(handshake: Handshake): IO[Unit] = ???
+  def start(handshake: Handshake): IO[Unit] = {
+    for {
+      chunk <- convertHandshakeToChunk(handshake)
+      _ <- socket.write(chunk)
+      response <- socket.readN(68)
+      _ <- IO.println(response)
+      responseHandshake <- convertAttemptToIO(handshakeCodec.decode(response.toBitVector))
+      _ <- IO.println(responseHandshake.value)
+    } yield ()
+  }
 }
 
 object PeerCommunication {
-  def apply(peerAddress: PeerAddress, activePeersRef: Ref[IO, Set[PeerAddress]]): Resource[IO, PeerCommunication] = {
-    Network[IO].client(SocketAddress.fromString(peerAddress.ip).get)
+  def apply(peerAddress: PeerAddress, activePeersRef: Ref[IO, Set[PeerAddress]])
+    : Resource[IO, PeerCommunication] = {
+    Network[IO]
+      .client(SocketAddress.fromString(peerAddress.ip).get)
       .flatMap { socket =>
-        val onClose = activePeersRef.update(currentPeerAddresses => currentPeerAddresses - peerAddress)
+        val onClose = activePeersRef.update(currentPeerAddresses =>
+          currentPeerAddresses - peerAddress)
         Resource.make(IO(PeerCommunication(socket)))(_ => onClose)
       }
   }
+
+  def convertHandshakeToChunk(handshake: Handshake): IO[Chunk[Byte]] = {
+    convertAttemptToIO(handshakeCodec.encode(handshake))
+      .map(bitVector => bitVector.toByteVector)
+      .map(byteVector => Chunk.byteVector(byteVector))
+  }
+
+  def convertAttemptToIO[A](attempt: Attempt[A]): IO[A] =
+    IO.fromTry(attempt.toTry)
 }
